7fa622f001e9516e28272a47fb4500076adec7b1
Timyu0527 <st970387@gmail.com>
2022-08-11 21:55:53 +0800

    online db

--- src/main/java/com/example/webapi/crawler/KeelungSightsCrawler.java
+++ src/main/java/com/example/webapi/crawler/KeelungSightsCrawler.java
@@ -7,7 +7,6 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
 import java.util.*;
